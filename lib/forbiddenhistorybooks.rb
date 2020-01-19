68bc159e70894fcc23b9c01ed621316522d34bef
Angelica  Thompson <i.thompson89@outlook.com>
2020-01-19 15:57:27 +0000

    requirements fhb.rb

--- lib/forbiddenhistorybooks.rb
+++ lib/forbiddenhistorybooks.rb
@@ -1,2 +1,8 @@
-require_relative "./forbiddenhistorybooks/version"
-require_relative './forbiddenhistorybooks/cli'
+require 'open-uri'
+require 'nokogiri'
+require 'pry'
+
+require_relative './cli/forbiddenhistorybooks/version'
+require_relative './cli/forbiddenhistorybooks/cli'
+require_relative './cli/cli'
+require_relative './cli/book'
