6aa4f47448529af91f88a09a1f6519e9aa94a160
Angelica  Thompson <i.thompson89@outlook.com>
2020-01-19 20:11:10 +0000

    comments about scraper

--- lib/cli/scraper.rb
+++ lib/cli/scraper.rb
@@ -1,8 +1,8 @@
 class Forbiddenhistorybooks::Scraper
   def self.scrape_books
     doc = Nokogiri::HTML(open("https://www.goodreads.com/list/show/75839.Forbidden_Bookshelf"))
-    #book_titles = doc.css(".bookTitle span") 
-    # book_titles = book_titles.map{|bk| bk.text}
+    #book_titles = doc.css(".bookTitle span") - we can put these two arrays(line4-5) together because you can call .map directly on the objects we returned from doc.css with .text being called in the .map we dont have to call it again on title 
+    # book_titles = book_titles.map{|bk| bk.text} 
     book_titles = doc.css(".bookTitle span").map{|bk| bk.text}
     
     book_titles.each do |title|
