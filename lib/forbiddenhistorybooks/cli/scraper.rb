46cbfc5e1a689aa25b9bcd2312ded5cc507de6b8
Angelica  Thompson <i.thompson89@outlook.com>
2020-01-19 16:44:03 +0000

    fixing line4-6 scraper.rb

--- lib/forbiddenhistorybooks/cli/scraper.rb
+++ lib/forbiddenhistorybooks/cli/scraper.rb
@@ -1,8 +1,9 @@
 class ForbiddenHistoryBooks::Scraper
   def scrape_books
     doc = Nokogiri::HTML(open("https://www.goodreads.com/list/show/75839.Forbidden_Bookshelf"))
-    book_titles = doc.css(".bookTitle span").collect {|atag| atag.css("a")} #does this need .text at the end? 
-      book_titles.each do |titles|
-      end
+    book_titles = doc.css(".bookTitle span") #.collect {|atag| atag.css("a")} #does this need .text at the end? 
+    book_titles.each do |title|
+      puts "title"
+    end
   end 
 end  
