b67edf8a5d41722f82a861347b46694fe32ce627
Angelica  Thompson <i.thompson89@outlook.com>
2020-01-19 16:11:05 +0000

    fixing spacing

--- lib/forbiddenhistorybooks/cli/scraper.rb
+++ lib/forbiddenhistorybooks/cli/scraper.rb
@@ -1,9 +1,8 @@
 class ForbiddenHistoryBooks::Scraper
   def scrape_books
-  
-  doc = Nokogiri::HTML(open("https://www.goodreads.com/list/show/75839.Forbidden_Bookshelf"))
-  book_titles = doc.css(".bookTitle span").collect {|atag| atag.css("a")} #does this need .text at the end? 
-  book_titles.each do |titles|
+    doc = Nokogiri::HTML(open("https://www.goodreads.com/list/show/75839.Forbidden_Bookshelf"))
+    book_titles = doc.css(".bookTitle span").collect {|atag| atag.css("a")} #does this need .text at the end? 
+      book_titles.each do |titles|
+      end
   end 
-
 end  
