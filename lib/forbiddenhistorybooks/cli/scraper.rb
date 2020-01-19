69c9f71d71bdfab936c80cc0e83cb5edefa31091
Angelica  Thompson <i.thompson89@outlook.com>
2020-01-19 16:09:37 +0000

    printing books to screen in scraper.rb

--- lib/forbiddenhistorybooks/cli/scraper.rb
+++ lib/forbiddenhistorybooks/cli/scraper.rb
@@ -3,6 +3,7 @@ class ForbiddenHistoryBooks::Scraper
   
   doc = Nokogiri::HTML(open("https://www.goodreads.com/list/show/75839.Forbidden_Bookshelf"))
   book_titles = doc.css(".bookTitle span").collect {|atag| atag.css("a")} #does this need .text at the end? 
+  book_titles.each do |titles|
   end 
 
 end  
