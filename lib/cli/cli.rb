7e674d0e7de164893dc81e80915601cffb244be7
Angelica  Thompson <i.thompson89@outlook.com>
2020-01-20 19:51:26 +0000

    cli.rb menu

--- lib/cli/cli.rb
+++ lib/cli/cli.rb
@@ -1,7 +1,18 @@
 class Forbiddenhistorybooks::CLI
   def call 
     Forbiddenhistorybooks::Scraper.scrape_books 
+    menu
   end   
   
+  def menu 
+    puts "Enter a number to find a Forbidden history book or type exit to exit:"
+    input = gets.strip.downcase
+    
+    if input.to_i > 0 || input.to_i < 29 
+      puts "you chose book with position #{input}"
+    else 
+      puts "Not sure what you're looking for, type a number between 1-28 or exit"
+    end
+  end 
   
 end 
